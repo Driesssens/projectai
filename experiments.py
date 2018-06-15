be98fec9bd518bce35176f24f3dd92a916124b43
Jasper Driessens <hsjdriessens@gmail.com>
2018-06-15 16:51:40 +0200

    I think I finished the matrix building logic

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 from pipeline import new_experiment, continue_experiment
 from configuration import Configuration
-from labeler import run_labeler
+from labeler import run_labeler, run_labeler_2
+import os
 
 
 def first_tests():
@@ -15,7 +16,32 @@ def test_labeler():
 
 def big_run():
     for i in range(0, 19):
-        run_labeler(input_folder=["input/csn_chopped/csn{}".format(i)], output_folder=["output/csn_chopped/csn{}".format(i)])
+        run_labeler(input_folder=["input/csn_chopped_stripped/csn{}".format(i)], output_folder=["output/csn_chopped_stripped/csn{}".format(i)])
 
 
-big_run()
+def big_run_2():
+    for i in range(0, 19):
+        run_labeler_2(input_folder=["input/csn_chopped/csn{}".format(i)], output_folder=["output/csn_chopped/csn{}".format(i)])
+
+
+def remove_bars():
+    for i in range(0, 19):
+        folder_name = "input/csn_chopped_stripped/csn{}".format(i)
+        for file_name in os.listdir(folder_name):
+            with open(folder_name + '/' + file_name, "r+") as open_file:
+                text = open_file.read().replace("_", " ")
+                open_file.seek(0)
+                open_file.write(text)
+
+
+def remove_quotes():
+    for i in range(0, 19):
+        folder_name = "output/csn_chopped/csn{}/temp/processed_arff_compatible".format(i)
+        for file_name in os.listdir(folder_name):
+            with open(folder_name + '/' + file_name, "r+") as open_file:
+                text = open_file.read().replace(',QUOTE}', ',"hoidoeikoehahahaha"}')
+                open_file.seek(0)
+                open_file.write(text)
+
+
+big_run_2()
--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,20 @@
 from datetime import datetime
 from configuration import Configuration
 import os
+import io
 from file_backlog import FileBacklog
+from collections import Counter
+from itertools import combinations
+from math import log
+from scipy.sparse import csc_matrix
+from pprint import pformat
+from scipy.sparse.linalg import svds
+import numpy as np
+from functools import partial
+from file_backlog import name
+import spacy
+import xml.etree.ElementTree as xml
+from spacy import symbols
 
 
 def new_experiment(configuration):
@@ -72,8 +85,74 @@ def label_messy(files, output_folder, configuration):
     pass
 
 
-def count_messy(files, output_folder, configuration):
-    pass
+def is_nsubj(word):
+    if word.dep_ == symbols.root:
+        return False
+    elif word.dep == symbols.nsubj:
+        return True
+    else:
+        return is_nsubj(word.head)
+
+
+def count_messy(output_folder, configuration):
+    full_unigram = Counter()
+    full_bigram = Counter()
+    weak_generic_unigram = Counter()
+    weak_generic_bigram = Counter()
+    weak_noun_unigram = Counter()
+    weak_noun_bigram = Counter()
+    strong_generic_unigram = Counter()
+    strong_generic_bigram = Counter()
+    strong_noun_unigram = Counter()
+    strong_noun_bigram = Counter()
+
+    nlp = spacy.load('en_core_web_sm')
+
+    for file_name in map(name, os.listdir('input/{}'.format(configuration.input_set))):
+        with io.open('input/{}/{}.txt'.format(configuration.input_set, file_name), encoding='utf-8') as txt_file:
+            document = nlp(txt_file.read())
+            for sentence in document.sents:
+                for word in sentence:
+                    full_unigram[word.lemma_] += 1
+                    for other in sentence:
+                        if word is not other:
+                            full_bigram[(word.lemma_, other.lemma_)] += 1
+
+                xml_root = xml.parse('messy_labeled/{}/{}.xml'.format(configuration.input_set, file_name)).getroot()
+
+                labels = [child[-1].attrib['seType'] for child in xml_root if (int(child.attrib['begin']) >= sentence.start_char) and (int(child.attrib['end']) <= sentence.end_char)]
+
+                if 'GENERIC_SENTENCE' in labels:
+                    set_of_labels = set(labels)
+                    only_generic = len(set_of_labels) == 1 and set_of_labels.pop() == 'GENERIC_SENTENCE'
+
+                    for word in sentence:
+                        is_nsubj_indeed = is_nsubj(word)
+
+                        weak_generic_unigram[word.lemma_] += 1
+
+                        if is_nsubj_indeed:
+                            weak_noun_unigram[word.lemma_] += 1
+
+                        for other in sentence:
+                            if word is not other:
+                                weak_generic_bigram[(word.lemma_, other.lemma_)] += 1
+
+                                if is_nsubj_indeed:
+                                    weak_noun_bigram[(word.lemma_, other.lemma_)] += 1
+
+                        if only_generic:
+                            strong_generic_unigram[word.lemma_] += 1
+
+                            if is_nsubj_indeed:
+                                strong_noun_unigram[word.lemma_] += 1
+
+                            for other in sentence:
+                                if word is not other:
+                                    strong_generic_bigram[(word.lemma_, other.lemma_)] += 1
+
+                                    if is_nsubj_indeed:
+                                        strong_noun_bigram[(word.lemma_, other.lemma_)] += 1
 
 
 def decorate_clean(the_file, output_folder, configuration):
